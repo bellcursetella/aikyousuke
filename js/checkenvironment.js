6ef837f25282fa1795353c0acf82372ae186549d
aiKyousuke <43588257+aiKyousuke@users.noreply.github.com>
2021-10-18 10:06:38 +0900

    2110181006

---
+++ js/checkenvironment.js
@@ -0,0 +1,17 @@
+javascript: ((func, scr) => {
+    scr = document.createElement("script");
+    scr.src = "//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+    scr.onload = () => {
+        func(jQuery.noConflict(true))
+    };
+    document.body.appendChild(scr)
+})(($) => {
+    "use strict";
+    
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    if (userAgent.indexOf("iphone") !== -1 || userAgent.indexOf("ipad") !== -1){
+        if (userAgent.indexOf("safari") !== -1 && userAgent.indexOf("chrome") == -1){
+            $('#text').text("※現在お使いの端末、ブラウザでは遊ぶことができません！※");
+        }
+    }
+});
